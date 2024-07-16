5e5fe5b29c8424a3e00a72a22e0f89191b4036dc
Bryan Rafosala <bryanrafosala@192.168.1.115>
2024-07-16 10:27:59 +0100

    Q4 - /api/articles/:article_id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -14,9 +14,19 @@ afterAll(() => {
 });
 
 
+describe("404 Requesting a path that does not exist.", () => {
+  test("404: When path does not exist", () => {
+    return request(app)
+      .get("/api/incorrect-path")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("404: Not Found");
+      });
+  });
+});
 
 
-describe("/api/topics", () => {
+describe("GET /api/topics", () => {
   test("GET 200: sends and array of topics", () => {
     return request(app)
       .get("/api/topics")
@@ -26,14 +36,14 @@ describe("/api/topics", () => {
         expect(topics.length).toBe(3);
         expect(Array.isArray(topics)).toBe(true);
         topics.forEach((topic) => {
-          expect(typeof topic.slug).toBe("string");
-          expect(typeof topic.description).toBe("string");
+          expect(topic.slug).toEqual(expect.any(String));
+          expect(topic.description).toEqual(expect.any(String));
         });
       });
   });
 });
 
-describe('/api', () => {
+describe('GET /api', () => {
   test('GET 200: Returns the information from the endpoints JSON file', () => {
       return request(app)
       .get('/api')
@@ -44,3 +54,43 @@ describe('/api', () => {
   });
 });
 
+describe("GET /api/articles/:article_id", () => {
+  test("GET 200: returns article with the specified article ID", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then((body) => {
+        const article = body._body.article
+
+        const articleFormat = 
+        {
+        article_id: expect.any(Number),
+        title: expect.any(String),
+        topic: expect.any(String),
+        author: expect.any(String),
+        body: expect.any(String),
+        created_at: expect.any(String),
+        votes: expect.any(Number),
+        article_img_url: expect.any(String),
+        }
+
+       expect(article).toMatchObject(articleFormat)
+      });
+  });
+  test("GET 404: returns appropriate error message when provided with a valid but non-existent article ID", () => {
+    return request(app)
+      .get("/api/articles/9999")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("No Article Found");
+      });
+  });
+  test("GET 400: returns correct error message if passed invalid article ID number", () => {
+    return request(app)
+      .get("/api/articles/bananas")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Bad Request");
+      });
+  });
+})
--- db/error.handling.js
+++ db/error.handling.js
@@ -1,7 +1,11 @@
-exports.customErrorHandler = ((err, request, response, next) => {
-    if(err.status && err.message){
-      response.status(err.status).send({msg: err.message})
-    } else{
-      next(err)
-    }
-  })  
+exports.customErrorHandler = (err, request, response, next) => {
+  if (err.status && err.message) {
+    response.status(err.status).send({ msg: err.message });
+  } else {
+    next(err);
+  }
+};
+
+exports.InternalServerError = (err, request, response, next) => {
+  response.status(500).send({ msg: "Internal Server Error" });
+};
