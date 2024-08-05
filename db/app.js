7289dfe5adf363a4c273a194a70124e0ede298e4
Bryan Rafosala <bryanrafosala@bryans-laptop.home>
2024-08-05 10:26:58 +0100

    allow cross origin resource sharing

--- db/app.js
+++ db/app.js
@@ -12,12 +12,12 @@ const {
   postCommentByArticleId,
   deleteCommentById
 } = require("./controller/comment.controller.js");
-
+const cors = require('cors');
 const { getUsers } = require("./controller/user.controller.js")
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 
 app.get("/api", fetchEndPoints);
 
